921fcce048f3530a13eea86f3e9ae97d3ad4b684
irwan123 <irwanrham@gmail.com>
2021-10-03 02:32:48 +0700

    download feature

--- lib/download.dart
+++ lib/download.dart
@@ -1,3 +1,5 @@
+import 'package:apptask/firebase_api.dart';
+import 'package:apptask/firebase_file.dart';
 import 'package:flutter/material.dart';
 
 class DownloadScreen extends StatefulWidget {
@@ -8,15 +10,85 @@ class DownloadScreen extends StatefulWidget {
 }
 
 class _DownloadScreenState extends State<DownloadScreen> {
-  late String payload;
+  late Future<List<FirebaseFile>> futureFiles;
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: const Text('Download File')),
-        body: const Center(
-            child: Text(
-          "Download",
-          style: TextStyle(fontSize: 30),
-        )));
+  void initState() {
+    super.initState();
+
+    futureFiles = FirebaseApi.listAll('files/');
   }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+      appBar: AppBar(title: const Text('Download File')),
+      body: FutureBuilder<List<FirebaseFile>>(
+        future: futureFiles,
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
+              return const Center(child: CircularProgressIndicator());
+            default:
+              if (snapshot.hasError) {
+                return const Center(child: Text('Data Gagal Dimuat'));
+              } else {
+                final files = snapshot.data!;
+
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    buildHeader(files.length),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    Expanded(
+                        child: ListView.builder(
+                            itemCount: files.length,
+                            itemBuilder: (context, index) {
+                              final file = files[index];
+
+                              return buildFile(context, file);
+                            }))
+                  ],
+                );
+              }
+          }
+        },
+      ));
+
+  Widget buildFile(BuildContext context, FirebaseFile file) => ListTile(
+      title: Text(
+        file.name,
+        style: const TextStyle(
+          fontWeight: FontWeight.bold,
+          decoration: TextDecoration.underline,
+          color: Colors.blue,
+        ),
+      ),
+      onTap: () async {
+        await FirebaseApi.downloadFile(file.ref);
+        final snackBar = SnackBar(
+          content: Text('Download ${file.name}'),
+        );
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      });
+
+  Widget buildHeader(int length) => ListTile(
+        tileColor: Colors.blue,
+        leading: const SizedBox(
+          width: 52,
+          height: 52,
+          child: Icon(
+            Icons.file_copy,
+            color: Colors.white,
+          ),
+        ),
+        title: Text(
+          '$length Files',
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+            color: Colors.white,
+          ),
+        ),
+      );
 }
